321a3952af49b521bc474fef7e629e068cd76c6e
Bruno Rodrigues Dos Reis Rossi Serra <74634382+brunoserra@users.noreply.github.com>
2021-01-17 18:39:13 -0300

    calculando

--- churrasco/calcular.js
+++ churrasco/calcular.js
@@ -1,11 +1,16 @@
-let homem = document.querySelector('input#qtdHomem');
-let mulher = document.querySelector('input#qtdMulher');
-let crianca = document.querySelector('input#qtdCrianca');
-let res = document.querySelector('div#resultado')
-let vetores = []
+//coletando inputs
+let homem = document.getElementById('#qtdHomem').value;
+let mulher = document.getElementById('#qtdMulher').value;
+let crianca = document.getElementById('#qtdCrianca').value;
+let res = document.getElementById('#resultado').value;
+
 
 
 
 function caclular() {
+    //soma das pessoas
+
+    let totalPessoas = Number(homem) + Number(mulher) + Number(crianca),
 
 }
+document.getElementById("total-pessoas").innerHTML = `${totalPessoas} pessoas`
