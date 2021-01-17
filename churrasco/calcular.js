ce85956f7ddcfab502ab83a91f485217fa99b648
Bruno Rodrigues Dos Reis Rossi Serra <74634382+brunoserra@users.noreply.github.com>
2021-01-17 18:22:01 -0300

    treinando

---
+++ churrasco/calcular.js
@@ -0,0 +1,11 @@
+let homem = document.querySelector('input#qtdHomem');
+let mulher = document.querySelector('input#qtdMulher');
+let crianca = document.querySelector('input#qtdCrianca');
+let res = document.querySelector('div#resultado')
+let vetores = []
+
+
+
+function caclular() {
+
+}
