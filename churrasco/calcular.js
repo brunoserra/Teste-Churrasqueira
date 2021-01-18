1dba3224ffc0da519b9fbdf97d9dff0cbf135325
Bruno Rodrigues Dos Reis Rossi Serra <74634382+brunoserra@users.noreply.github.com>
2021-01-17 22:44:42 -0300

    calculando

--- churrasco/calcular.js
+++ churrasco/calcular.js
@@ -7,7 +7,7 @@ function calculo() {
     let cerveja = document.getElementById(`cervejas`)
     let refrigerante = document.getElementById(`refrigerantes`)
     let acpto = document.getElementById(`acompanhamento`)
-    let result = document.getElementById(`resultado`)
+    let res = document.getElementById(`res`)
 
     //fazendo os calculos
     if (homem.value.length == 0 || mulher.value.length == 0 || crianca.value.length == 0 || cerveja.value.length == 0 || refrigerante.value.length == 0) {
@@ -19,5 +19,16 @@ function calculo() {
         let qtdC = Number(crianca.value)
         let qtdBeer = Number(cerveja.value)
         let qtdRefri = Number(refrigerante.value)
+
+        //Quantidade de Pessoas
+        totalPessoas = qtdH + qdtM + qtdC
+            //Calculo da Carne
+        totalCarne = (qtdH * 0.63) + (qtdM * 0.48) + (qtdC * 0.350)
+            // Acompanhementos
+        if (acpto.checked) {
+            totalac = (qtdH * 0.450) + (qtM * 0.350) + (qtdC * 0.250)
+
+        }
     }
+
 }
